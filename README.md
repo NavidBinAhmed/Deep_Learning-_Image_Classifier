ac53d95f41a168f2d101570377135715c9fa8b85
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2022-12-18 19:53:41 +0600

    This commit if for README.md

---
+++ README.md
@@ -0,0 +1,35 @@
+
+CNN-based Image detection system
+
+copyright @navidbinahmed 2022
+
+
+## steps 
+1. Created a new conda environment (imageclassifier)
+2. Downloaded dataset
+3. Model Building
+    i.   Preprocessing data
+    ii.  EDA/ feature engineering
+    iii. Model building
+    iv.  Saved model as a .h5 file
+4. Customized app building using Flask
+5. Install Docker Desktop with supporting tools (WSL 2)
+6. Pushed the repo to GitHub using git CLI (and Docker)
+    i. Configuaration commands:
+         git config --global user.name
+         git config --global user.email
+
+
+## tools and tech used
+1. Jupyter Notebook & Anaconda
+2. VS Code
+3. Docker Desktop
+4. WSL 2
+5. GitHub
+6. Git CLI
+
+## Programming languages used
+1. Python, v3.9
+2. Python Flask Framwork
+3. HTML customised
+4. CSS customised
---
+++ a2model_webapp.py
@@ -0,0 +1,88 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+from tensorflow import keras
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+
+# from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+modelpath = 'a2model_resnet50.h5'
+
+# import os
+# MODEL_PATH = os.path.join(os.path.dirname(__file__), "a2model_resnet50.h5")
+
+# Load your trained model
+model = load_model(modelpath)
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(256, 256))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    ## Scaling
+    x=x/255
+    x = np.expand_dims(x, axis=0)
+   
+
+   
+
+    prediction = model.predict(x)
+    prediction=np.argmax(prediction, axis=1)
+    if prediction==0:
+        prediction="This is berry"
+    elif prediction==1:
+        prediction="This is bird"
+    elif prediction==2:
+        prediction="This is dog"
+    else:
+        prediction="This is flower"
+    
+    
+    return prediction
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        prediction = model_predict(file_path, model)
+        result=prediction
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
